6a5e2cad1088d2ebde44697aba3851bfb56d1c87
tatsuya-kato08 <78143445+tatsuya-kato08@users.noreply.github.com>
2021-02-22 20:06:37 +0900

    ユーザー管理機能の実装(修正2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password,:password_confirmation, format:{with: /\A[a-zA-Z0-9]+\z/}
-
+  validates  :password,:password_confirmation, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   with_options presence: true do
     validates :nickname
 
