bb6ec72a94f0a25b1e71235ddf6c1f8fd5bfd941
tatsuya-kato08 <78143445+tatsuya-kato08@users.noreply.github.com>
2021-03-02 12:27:02 +0900

    販売価格の保存できる値の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
     validates :name
     validates :description
     validates :image
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than: 9_999_999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   with_options numericality: { other_than: 1 } do
